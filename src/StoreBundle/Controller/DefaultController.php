4fd9d5f236c6a15c0403e8df8642cd44836e33ee
Paul Novikov <paul.novikov@mentax.pl>
2016-04-18 16:08:46 +0200

    adding logout option

--- src/StoreBundle/Controller/DefaultController.php
+++ src/StoreBundle/Controller/DefaultController.php
@@ -17,6 +17,9 @@ class DefaultController extends Controller
         define('__TRANSDIR__', $this->container->getParameter('kernel.root_dir') . '/Resources/translations/index/');
 
         $request->cookies->set('clientLang', $lang);
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+
+        var_dump($user);
 
         return $this->render('StoreBundle:Store:index.html.twig', include_once(__TRANSDIR__ . $lang . '.php'));
 
