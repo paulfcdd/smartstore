10e6881638823afc0df3549a190ff5cc11ae556e
Paul Novikov <paul.novikov@mentax.pl>
2016-04-22 15:33:10 +0200

    develop CPanel

---
+++ web/js/app.js
@@ -0,0 +1,49 @@
+'use strict';
+
+angular
+
+    .module('cpanel', [
+        'ngRoute'
+    ])
+
+    .config(['$routeProvider', function ($routeProvider) {
+
+        $routeProvider.when('cpanel/:categoriy', {
+                templateUrl: 'src/StoreBundle/Resources/views/Store/test.html',
+                controller: 'AppCtrl'
+        });
+
+        //$routeProvider.when('/', {
+        //    templateUrl: 'src/tpl/login.tpl.html',
+        //    controller: 'AppCtrl'
+        //}).when('/recovery', {
+        //    templateUrl: 'src/tpl/recovery.tpl.html',
+        //    controller: 'AppCtrl'
+        //}).when('/:category', {
+        //    templateUrl: 'src/tpl/category.tpl.html',
+        //    controller: 'CategoryCtrl'
+        //}).when('/:category/:subcategory', {
+        //    templateUrl: 'src/tpl/subcategory.tpl.html',
+        //    controller: 'SubcatCtrl'
+        //}).when('/:category/:subcategory/:id', {
+        //    templateUrl: 'src/tpl/singleProject.tpl.html',
+        //    controller: 'singleProject'
+        //}).when('/:category/:subcategory/:id/:detail', {
+        //    templateUrl: 'src/tpl/baustelle/detail.tpl.html',
+        //    controller: 'baustelleDet'
+        //});
+
+    }])
+
+    .run(function run() {
+
+    })
+
+
+    .controller('AppCtrl', function AppCtrl($scope) {
+        $scope.test = 'hello';
+
+        $scope.test = function() {
+            alert('kek');
+        }
+    });
