88a2d83fa2819ded6ffd621b00f8834f8c41e3c0
Paul Novikov <paul.novikov@mentax.pl>
2016-06-09 13:10:52 +0200

    вывод категорий на главную страницу

--- src/StoreBundle/Controller/CategoryController.php
+++ src/StoreBundle/Controller/CategoryController.php
@@ -15,15 +15,40 @@ use Symfony\Component\HttpFoundation\Request;
 
 class CategoryController extends Controller
 {
-    public function categoryAction($title) {
+    public function categoryAction($title)
+    {
 
         $sysLang = new Session();
 
 //        var_dump($sysLang->get('selectedLang'));
 
         return $this->render('Store/tpl/category.html.twig', array(
-           'title' => $title
+            'title' => $title
         ));
 
+    }
+
+    public function renderCategoryListAction(Request $request)
+    {
+
+        $currentLang = $request->get('currentLang');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $get = $em->createQueryBuilder();
+
+        $get
+            ->select('c.cKeyword', 'ct.translation')
+            ->from('StoreBundle\Entity\Categories', 'c')
+            ->innerJoin('StoreBundle\Entity\CTranslations', 'ct', 'WITH', 'c.cId = ct.cId')
+            ->where("ct.langCode = '$currentLang'");
+
+        $query = $get->getQuery();
+
+        $categories = $query->getResult();
+
+        return $this->render('StoreBundle:Store:categoryList.html.twig', array(
+            'categories' => $categories
+        ));
     }
 }
